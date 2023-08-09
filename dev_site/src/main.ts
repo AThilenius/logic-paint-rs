bffce6a591399517a809c9fdd242100803c6da39
Alec Thilenius <alec@thilenius.com>
2023-08-08 17:15:05 -0700

    Thinking and fix dev page

--- dev_site/src/main.ts
+++ dev_site/src/main.ts
@@ -1,14 +1,15 @@
-import init, { LogicPaint } from 'crate';
+import init, { LogicPaint } from "crate";
 
 async function main() {
   await init();
 
   const logicPaint = new LogicPaint(
-    document.getElementById('root') as HTMLCanvasElement
+    document.getElementById("root") as HTMLCanvasElement,
+    () => {},
+    () => {},
+    () => {},
+    () => {}
   );
-
-  // DEV
-  (window as any).logicPaint = logicPaint;
 }
 
 void main();
