d8deb07ab332706ceb6bdee85c72561a73edd2a2
Alec Thilenius <alec@thilenius.com>
2024-10-20 10:31:47 -0700

    WIP - Adding camera controls

--- src/wgl2/uniform.rs
+++ src/wgl2/uniform.rs
@@ -33,6 +33,8 @@ impl SetUniformType<u32> for Uniform<u32> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: u32) {
         if let Some(location) = &self.location {
             ctx.uniform1ui(Some(&location), v);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -41,6 +43,8 @@ impl SetUniformType<i32> for Uniform<i32> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: i32) {
         if let Some(location) = &self.location {
             ctx.uniform1i(Some(&location), v);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -49,6 +53,8 @@ impl SetUniformType<f32> for Uniform<f32> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: f32) {
         if let Some(location) = &self.location {
             ctx.uniform1f(Some(&location), v);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -57,6 +63,8 @@ impl SetUniformType<Vec2> for Uniform<Vec2> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: Vec2) {
         if let Some(location) = &self.location {
             ctx.uniform2f(Some(&location), v.x, v.y);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -65,6 +73,8 @@ impl SetUniformType<IVec2> for Uniform<IVec2> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: IVec2) {
         if let Some(location) = &self.location {
             ctx.uniform2i(Some(&location), v.x, v.y);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -73,6 +83,8 @@ impl SetUniformType<Vec3> for Uniform<Vec3> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: Vec3) {
         if let Some(location) = &self.location {
             ctx.uniform3f(Some(&location), v.x, v.y, v.z);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -81,6 +93,8 @@ impl SetUniformType<IVec3> for Uniform<IVec3> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: IVec3) {
         if let Some(location) = &self.location {
             ctx.uniform3i(Some(&location), v.x, v.y, v.z);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -89,6 +103,8 @@ impl SetUniformType<Vec4> for Uniform<Vec4> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: Vec4) {
         if let Some(location) = &self.location {
             ctx.uniform4f(Some(&location), v.x, v.y, v.z, v.w);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -97,6 +113,8 @@ impl SetUniformType<IVec4> for Uniform<IVec4> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: IVec4) {
         if let Some(location) = &self.location {
             ctx.uniform4i(Some(&location), v.x, v.y, v.z, v.w);
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
@@ -105,6 +123,8 @@ impl SetUniformType<Mat4> for Uniform<Mat4> {
     fn set(&self, ctx: &WebGl2RenderingContext, v: Mat4) {
         if let Some(location) = &self.location {
             ctx.uniform_matrix4fv_with_f32_array(Some(&location), false, v.as_ref());
+        } else {
+            warn!("Program uniform location missing");
         }
     }
 }
